4f2b975b4618be4238c50f19a6a44c6ff43fe05c
Wataru Kanzaki <kanzwataru@gmail.com>
2018-05-27 12:32:58 -0400

    * made build script

--- package.sh
+++ package.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
+tag=`git describe --tag`
+outer_name=eevee-toon_$tag.zip
+inner_name=eevee-toon-addon.zip
 
+mkdir -p build/_temp
+mkdir -p build/_temp/eevee-toon
+cp -R addon/* build/_temp/eevee-toon/
+cp -R resources/* build/_temp/eevee-toon/
+
+cd build/_temp/eevee-toon
+zip -r ../$inner_name *
+cd ../../../
+
+rm -R build/_temp/eevee-toon
+cp -R extras/* build/_temp/
+cd build/_temp
+zip -r ../$outer_name *
+cd ../../
