08a46081404546f83dfc48f75f6a1e028db6e15c
Wataru Kanzaki <kanzwataru@gmail.com>
2018-07-15 14:14:28 -0400

    * fixed build script

--- package.sh
+++ package.sh
@@ -1,22 +1,25 @@
-#!/bin/sh
-
+#!/bin/bash
 tag=`git describe --tag`
 outer_name=eevee-toon_$tag.zip
 inner_name=eevee-toon-addon.zip
 
+rm -Rf resources/*.blend1
+rm -Rf extras/*.blend1
+
 mkdir -p build/_temp
-mkdir -p build/_temp/eevee-toon
-cp -R addon/* build/_temp/eevee-toon/
-cp -R resources/* build/_temp/eevee-toon/
+mkdir -p build/_temp/_inner/eevee-toon
+cp -R addon/eevee-toon/* build/_temp/_inner/eevee-toon/
+cp -R resources/* build/_temp/_inner/eevee-toon/
 
-cd build/_temp/eevee-toon
+pushd build/_temp/_inner
 zip -r ../$inner_name *
-cd ../../../
+popd
 
-rm -R build/_temp/eevee-toon
+rm -R build/_temp/_inner
 cp -R extras/* build/_temp/
-cd build/_temp
+pushd build/_temp
+rm -Rf ../$outer_name      # make sure we don't append to existing archive
 zip -r ../$outer_name *
-cd ../../
+popd
 
 rm -R build/_temp
